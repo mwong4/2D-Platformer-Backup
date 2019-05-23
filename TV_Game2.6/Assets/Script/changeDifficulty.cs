5a761faa7e9c1d69130a694f598a216d59714354
MW <43390816+mwong4@users.noreply.github.com>
2019-05-23 11:30:05 -0400

    Added some more upgrades

--- TV_Game2.6/Assets/Script/changeDifficulty.cs
+++ TV_Game2.6/Assets/Script/changeDifficulty.cs
@@ -6,11 +6,28 @@ public class changeDifficulty : MonoBehaviour {
 
 	public GameObject myCamera;
 
+	public GameObject[] slowPoints;
+	public GameObject[] fastPoints;
+
+
 	// Use this for initialization
 	void Start () {
 		myCamera = GameObject.FindGameObjectWithTag ("MainCamera");
 		Mover movingScript = myCamera.GetComponent<Mover> ();
 		movingScript.speed = 6f;
+
+		slowPoints = GameObject.FindGameObjectsWithTag ("slowerCameraPoints");
+
+		foreach (GameObject x in slowPoints) {
+			Waypoint waypoint = x.GetComponent<Waypoint> ();
+			waypoint.speedOut = 1.5f;
+		}
+
+		fastPoints = GameObject.FindGameObjectsWithTag ("fasterCameraPoints");
+		foreach (GameObject x in fastPoints) {
+			Waypoint waypoint = x.GetComponent<Waypoint> ();
+			waypoint.speedOut = 5;
+		}
 	}
 	/*	public void changeSpeed ( GameObject myCamera, Mover movingScript )
 	{
